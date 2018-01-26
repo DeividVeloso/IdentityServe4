18be9db681065029e045ad86809c1cb5f9e54fbd
Deivid Veloso <veloso.deivid@gmail.com>
2018-01-26 17:10:53 -0200

    first commit

---
+++ src/Console.ClientUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Console.ClientUI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+             CallAPIAsync();
+            System.Console.ReadLine();
+        }
+        
+        static async void CallAPIAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                System.Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "Console.ClientUI", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("API.User");
+
+            if (tokenResponse.IsError)
+            {
+                System.Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            System.Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                System.Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                System.Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
---
+++ src/IdentityServer/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Defino as cconfigurações do CLiente
+            /*
+                Exemplo:
+                    - o Console.ClientUI vai acessar a API de Usuário
+            */
+            var clients = new List<Client>();
+            clients.Add(new Client()
+            {
+                ClientId = "Console.ClientUI",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "API.User" } //Permitido ter acesso a API.usuário
+            });
+
+            // Adiciona a autorização para API's
+            var apiResources = new List<ApiResource>();
+            apiResources.Add(new ApiResource("API.User", "Manager User"));
+
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(apiResources)
+                .AddInMemoryClients(clients);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
